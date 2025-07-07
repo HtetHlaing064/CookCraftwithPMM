2c42bd3c40369c7ed8ebc1a154601924fce01f0d
Wai Yan Hein <hwaiyan913@gmail.com>
2025-07-07 16:28:49 +0630

    add

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import AppLayout from "./components/AppLayout";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
+import StyledRoot from "./StyledRoot";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,7 +23,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <AppRouterCacheProvider>
+          <StyledRoot>
+            <AppLayout>{children}</AppLayout>
+          </StyledRoot>
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
